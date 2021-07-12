927d87888526ae2eeab3530f29e83dcab2e75855
Danilo Dell'Orco <danilodellorcolp@gmail.com>
2021-07-12 19:03:28 +0200

    Terminated

--- src/main/java/dataset/CreateDataset.java
+++ src/main/java/dataset/CreateDataset.java
@@ -82,7 +82,7 @@ public class CreateDataset {
 	
 	public static void main(String[] args) throws GitAPIException, IOException {
 		CreateDataset datasetCreator = new CreateDataset();
-//		datasetCreator.create(Parameters.BOOKKEEPER);
+		datasetCreator.create(Parameters.BOOKKEEPER);
 		datasetCreator.create(Parameters.SYNCOPE);
 	}
 }
